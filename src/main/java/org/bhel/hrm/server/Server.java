23a5830b519353492e5d1d44e1633912bf9bd4be
KhooLayHan <khoolayhan@github.com>
2025-10-17 10:07:41 +0800

    feat: Added starter template Server and ServerLauncher class for server initiation.

--- src/main/java/org/bhel/hrm/server/Server.java
+++ src/main/java/org/bhel/hrm/server/Server.java
@@ -1,4 +1,37 @@
 package org.bhel.hrm.server;
 
-public class Server {
+import org.bhel.hrm.common.models.Employee;
+import org.bhel.hrm.common.services.Service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server extends UnicastRemoteObject implements Service {
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+
+    protected Server() throws RemoteException {
+        super();
+    }
+
+    @Override
+    public String registerEmployee(String firstName, String lastName, String icPassport) throws RemoteException {
+        logger.info("Client requested to register employees: {}", firstName);
+
+//         TODO: Will need to implement calling the DatabaseManager when saving the employee.
+        return "";
+    }
+
+    @Override
+    public Employee getEmployeeDetails(String employeeId) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public boolean authenticateUser(String username, String password) throws RemoteException {
+        return false;
+    }
 }
--- src/main/java/org/bhel/hrm/server/ServerLauncher.java
+++ src/main/java/org/bhel/hrm/server/ServerLauncher.java
@@ -1,4 +1,32 @@
 package org.bhel.hrm.server;
 
+import org.bhel.hrm.common.services.Service;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ServerLauncher {
+    private static final Logger logger = LoggerFactory.getLogger(ServerLauncher.class);
+
+    public static void main(String[] args) {
+        DatabaseManager.initializeDatabase();
+
+        logger.info("HRM Server is starting up...");
+
+        try {
+            Server server = new Server();
+
+            Registry registry = LocateRegistry.createRegistry(1099);
+
+            registry.rebind(Service.SERVICE_NAME, server);
+
+            logger.info("Server is running and waiting for client connections...");
+       } catch (Exception e) {
+            logger.error("Server exception: {}", e.toString());
+            logger.error("A fatal error occurred during startup {}", e.getMessage());
+        }
+    }
 }
