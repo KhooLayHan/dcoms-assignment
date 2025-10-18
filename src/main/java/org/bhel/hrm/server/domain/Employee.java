e0a0948289bced1bfac0e8b587eedd910f0edabf
KhooLayHan <khoolayhan@github.com>
2025-10-18 14:33:10 +0800

    feat: Added new Employee Domain and Mapper class for EmployeeDTO signature from client.

--- src/main/java/org/bhel/hrm/server/domain/Employee.java
+++ src/main/java/org/bhel/hrm/server/domain/Employee.java
@@ -1,4 +1,22 @@
 package org.bhel.hrm.server.domain;
 
 public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String icPassport;
+
+    // You could add complex, non-serializable fields here in the future
+    // private PerformanceReview lastReview;
+
+    public Employee() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getIcPassport() { return icPassport; }
+    public void setIcPassport(String icPassport) { this.icPassport = icPassport; }
 }
--- src/main/java/org/bhel/hrm/server/mapper/EmployeeMapper.java
+++ src/main/java/org/bhel/hrm/server/mapper/EmployeeMapper.java
@@ -1,4 +1,48 @@
 package org.bhel.hrm.server.mapper;
 
+import org.bhel.hrm.common.dtos.EmployeeDTO;
+import org.bhel.hrm.server.domain.Employee;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class EmployeeMapper {
+    private EmployeeMapper() {
+        throw new UnsupportedOperationException("EmployeeMapper class");
+    }
+
+    // Maps a single Domain object to a DTO
+    public static EmployeeDTO toDto(Employee employee) {
+        if (employee == null)
+            return null;
+
+        EmployeeDTO dto = new EmployeeDTO();
+
+        dto.setId(employee.getId());
+        dto.setFirstName(employee.getFirstName());
+        dto.setLastName(employee.getLastName());
+        dto.setIcPassport(employee.getIcPassport());
+
+        return dto;
+    }
+
+    // Maps a single DTO to a Domain object
+    public static Employee toDomain(EmployeeDTO dto) {
+        if (dto == null)
+            return null;
+
+        Employee employee = new Employee();
+
+        employee.setId(dto.getId());
+        employee.setFirstName(dto.getFirstName());
+        employee.setLastName(dto.getFirstName());
+        employee.setIcPassport(dto.getIcPassport());
+
+        return employee;
+    }
+
+    // Converts a list of Domain objects to a list of DTOs
+    public static List<EmployeeDTO> toDtoList(List<Employee> employees) {
+        return employees.stream().map(EmployeeMapper::toDto).collect(Collectors.toList());
+    }
 }
