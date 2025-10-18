af109780f9fa8119f053dcd3044c736ee97cb02c
KhooLayHan <khoolayhan@github.com>
2025-10-19 03:17:58 +0800

    feat: Added LoginController.java and MainClient implementation, currently not truly working yet.

--- src/main/java/org/bhel/hrm/client/MainClient.java
+++ src/main/java/org/bhel/hrm/client/MainClient.java
@@ -1,4 +1,25 @@
 package org.bhel.hrm.client;
 
-public class MainClient {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class MainClient extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        // Loads the login view as the first screen.
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("view/LoginView.fxml")));
+        primaryStage.setTitle("BHEL HRM System – Login");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/org/bhel/hrm/client/controllers/LoginController.java
+++ src/main/java/org/bhel/hrm/client/controllers/LoginController.java
@@ -1,4 +1,54 @@
 package org.bhel.hrm.client.controllers;
 
+import org.bhel.hrm.common.services.IService;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class LoginController {
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+
+    @FXML private TextField usernameField;
+    @FXML private TextField passwordField;
+
+    private IService service;
+
+    public LoginController() {
+        // Connects to the RMI service when the controller is created.
+        try {
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+            this.service = (IService) registry.lookup(IService.SERVICE_NAME);
+        } catch (Exception e) {
+            logger.error("Client exception: Could not connect to the RMI service. {%s}", e);
+        }
+    }
+
+    @FXML
+    protected void handleLoginButtonAction() {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // TODO: Validate input parameters locally.
+
+        try {
+            if (service.isUserAuthenticated(username, password)) {
+                logger.info("Login successful!");
+
+                // TODO: Closes the login window and opens the main dashboard.
+            } else {
+                logger.info("Login failed!");
+                // TODO: Shows a "Login failed" error message to the user.
+            }
+        } catch (Exception e) {
+            logger.error("Error during the authentication: {}", e.getMessage());
+
+            // TODO: Shows a connection error dialog.
+        }
+    }
 }
