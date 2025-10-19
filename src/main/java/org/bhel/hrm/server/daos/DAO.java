96171a36e03c688f48dd20b94a613c545055707e
KhooLayHan <khoolayhan@github.com>
2025-10-19 09:59:44 +0800

    build: Added docker-compose file for running MySQL image, with dockerignore file.

--- src/main/java/org/bhel/hrm/server/daos/DAO.java
+++ src/main/java/org/bhel/hrm/server/daos/DAO.java
@@ -1,4 +1,21 @@
 package org.bhel.hrm.server.daos;
 
-public interface DAO {
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO<T> {
+    Optional<T> find(long id);
+    List<T> findAll();
+
+    void save(T t);
+    void saveAll();
+
+    int count();
+
+    void update(T t, String[] params);
+
+    void delete(T t);
+    void deleteAllById(Iterable<? extends T> ids);
+
+    boolean existsById();
 }
