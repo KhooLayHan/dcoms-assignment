ca4c46a5f8e6c1dedaea56f729cec72132e3c36c
KhooLayHan <khoolayhan@github.com>
2025-10-20 18:01:26 +0800

    feat: Added generic parent DAO interface for extending to other DAOs data models implementation.

--- src/main/java/org/bhel/hrm/server/daos/DAO.java
+++ src/main/java/org/bhel/hrm/server/daos/DAO.java
@@ -3,19 +3,19 @@ package org.bhel.hrm.server.daos;
 import java.util.List;
 import java.util.Optional;
 
-public interface DAO<T> {
-    Optional<T> find(long id);
+public interface DAO<T, ID> {
+    Optional<T> findById(ID id);
     List<T> findAll();
 
-    void save(T t);
-    void saveAll();
+    T save(T t);
+    void saveAll(Iterable<T> entities);
 
     int count();
 
-    void update(T t, String[] params);
+    void update(T entity);
 
-    void delete(T t);
-    void deleteAllById(Iterable<? extends T> ids);
+    void delete(T entity);
+    void deleteById(ID id);
 
-    boolean existsById();
+    boolean existsById(ID id);
 }
