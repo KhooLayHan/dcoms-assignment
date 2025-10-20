3f42b24bfc5bca720476b60469fdb6972cbcbf38
KhooLayHan <khoolayhan@github.com>
2025-10-20 10:04:36 +0800

    feat: Updated to use record classes instead for immutability.

--- src/main/java/org/bhel/hrm/common/dtos/EmployeeDTO.java
+++ src/main/java/org/bhel/hrm/common/dtos/EmployeeDTO.java
@@ -1,48 +1,10 @@
 package org.bhel.hrm.common.dtos;
 
-import java.io.Serial;
 import java.io.Serializable;
 
-public class EmployeeDTO implements Serializable {
-    @Serial
-    private static final long serialVersionUID = 1L;
-
-    private int id;
-    private String firstName;
-    private String lastName;
-    private String icPassport;
-
-    public EmployeeDTO() {}
-
-    public String getIcPassport() {
-        return icPassport;
-    }
-
-    public void setIcPassport(String icPassport) {
-        this.icPassport = icPassport;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-}
+public record EmployeeDTO(
+    int id,
+    String firstName,
+    String lastName,
+    String icPassport
+) implements Serializable {}
