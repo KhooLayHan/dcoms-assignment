a2a9e2038c3ca0b30ca7bb82c9887360a1a34492
KhooLayHan <khoolayhan@github.com>
2025-10-20 16:11:54 +0800

    feat: Added NewEmployeeRegistrationDTO for HRMService interface registerNewEmployee method.

--- src/main/java/org/bhel/hrm/common/dtos/NewEmployeeRegistrationDTO.java
+++ src/main/java/org/bhel/hrm/common/dtos/NewEmployeeRegistrationDTO.java
@@ -1,4 +1,19 @@
 package org.bhel.hrm.common.dtos;
 
-public class NewEmployeeRegistrationDTO {
-}
+import java.io.Serializable;
+
+/**
+ * A specialized DTO to carry all necessary information for registering a new employee,
+ * which includes creating both a User account and an Employee profile.
+ */
+public record NewEmployeeRegistrationDTO(
+    // User account details
+    String username,
+    String initialPassword,
+    UserDTO.Role role,
+
+    // Employee profile details
+    String firstName,
+    String lastName,
+    String icPassport
+) implements Serializable {}
