324fc73cb6dbba84e7930114c6b7faf6e97a4963
KhooLayHan <khoolayhan@github.com>
2025-10-20 18:07:02 +0800

    feat: Reduced the number of implementations necessary of generic DAO interface.

--- src/main/java/org/bhel/hrm/server/daos/DAO.java
+++ src/main/java/org/bhel/hrm/server/daos/DAO.java
@@ -3,19 +3,44 @@ package org.bhel.hrm.server.daos;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * A generic Data Access Object (DAO) interface defining the standard CRUD
+ * operations for all domain entities.
+ *
+ * @param <T> The type of the domain entity (e.g., Employee, User).
+ * @param <ID> The type of the entity's primary key (e.g., Integer).
+ */
 public interface DAO<T, ID> {
+    /**
+     * Retrieves an entity by its ID.
+     * @param id The ID of the entity.
+     * @return An Optional containing the entity if found, otherwise an empty Optional.
+     */
     Optional<T> findById(ID id);
+
+    /**
+     * Retrieves all entities of this type.
+     * @return A List of all entities.
+     */
     List<T> findAll();
 
-    T save(T t);
-    void saveAll(Iterable<T> entities);
+    /**
+     * Saves a given entity. Use the returned instance for further operations
+     * as the save operation might have changed the entity instance completely.
+     * If the entity is new, it will be inserted. If it exists, it will be updated.
+     * @param entity The entity to save.
+     */
+    T save(T entity);
 
-    int count();
-
-    void update(T entity);
-
-    void delete(T entity);
+    /**
+     * Deletes an entity by its ID.
+     * @param id The ID of the entity to delete.
+     */
     void deleteById(ID id);
 
-    boolean existsById(ID id);
+    /**
+     * Returns the total number of entities.
+     * @return The count of entities.
+     */
+    int count();
 }
