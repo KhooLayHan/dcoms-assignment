d2beef0bf7f9cab264e6fa7b7f0b009fa9971c11
KhooLayHan <khoolayhan@github.com>
2025-11-10 14:09:10 +0800

    feat: Added new FXML filepaths constants for reusability.

--- src/main/java/org/bhel/hrm/client/constants/FXMLPaths.java
+++ src/main/java/org/bhel/hrm/client/constants/FXMLPaths.java
@@ -1,4 +1,15 @@
 package org.bhel.hrm.client.constants;
 
 public class FXMLPaths {
+    public static final String BASE
+        = "/org/bhel/hrm/client/view";
+
+    public static final String LOGIN
+        = BASE + "/LoginView.fxml";
+
+    public static final String EMPLOYEE_MANAGEMENT
+        = BASE + "/EmployeeManagementView.fxml";
+
+    public static final String EMPLOYEE_FORM
+        = BASE + "/dialogs/EmployeeFormView.fxml";
 }
